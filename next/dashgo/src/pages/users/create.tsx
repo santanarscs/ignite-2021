31d428cc0af242829839cd724ec000369901f811
Raphael Santana <raphaelstn@gmail.com>
2021-05-06 16:20:42 -0300

    set up navigation

--- next/dashgo/src/pages/users/create.tsx
+++ next/dashgo/src/pages/users/create.tsx
@@ -2,6 +2,7 @@ import { Box, Button, Divider, Flex, Heading, HStack, SimpleGrid, VStack } from 
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
 import { Input } from '../../components/Form/Input'
+import Link from "next/link";
 export default function CreateUser() {
   return(
     <Box>
@@ -30,7 +31,9 @@ export default function CreateUser() {
           </VStack>
           <Flex mt="8" justify="flex-end">
             <HStack spacing="4">
-              <Button colorScheme="whiteAlpha">Cancelar</Button>
+              <Link href="/users/create" passHref>
+                <Button colorScheme="whiteAlpha">Cancelar</Button>
+              </Link>
               <Button colorScheme="pink">Salvar</Button>
             </HStack>
           </Flex>
