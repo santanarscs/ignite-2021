31640191e3204dc9e845091ffd36c45dacb76bab
Raphael Santana <raphaelstn@gmail.com>
2021-05-14 11:18:24 -0300

    configure ssr

--- next/dashgo/src/services/hooks/useUsers.ts
+++ next/dashgo/src/services/hooks/useUsers.ts
@@ -1,7 +1,7 @@
-import { useQuery } from "react-query";
+import { useQuery, UseQueryOptions } from "react-query";
 import { api } from "../api";
 
-type User = {
+export type User = {
   id: string;
   name: string;
   email: string;
@@ -35,8 +35,9 @@ export async function getUsers(page: number): Promise<GetUsersResponse> {
   return {users, totalCount};
 }
 
-export function useUsers(page: number) {
+export function useUsers(page: number, options: UseQueryOptions) {
   return useQuery(['users', page], () => getUsers(page), {
-    staleTime: 1000 * 60 * 10
+    staleTime: 1000 * 60 * 10,
+    ...options
   })
 }
