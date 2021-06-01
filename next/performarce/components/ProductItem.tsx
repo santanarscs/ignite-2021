89a0576a73b4b0fb7c5e398be12d07d1bc6b79fd
Raphael Santana <raphaelstn@gmail.com>
2021-06-01 14:12:12 -0300

    init app performace

---
+++ next/performarce/components/ProductItem.tsx
@@ -0,0 +1,14 @@
+interface ProductItemProps {
+  product: {
+    id: number,
+    price: number,
+    title: string,
+  }
+}
+export function ProductItem({product}: ProductItemProps) {
+  return (
+    <div>
+      {product.title} - <strong>{product.price}</strong>
+    </div>
+  )
+}
---
+++ next/performarce/server.js
@@ -0,0 +1,14 @@
+module.exports = () =>  {
+  const data = {
+    products: []
+  }
+  for (let i = 0; i < 1000; i++) {
+    data.products.push({
+      id: i + 1,
+      price: 30,
+      title: `Camisa ${i + 1}`
+    })
+  }
+  return data;
+
+}
